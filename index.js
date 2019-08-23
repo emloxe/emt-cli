17cbe0e4a6228ce7a7ea439ad48d0a739ef32026
Zhang WJ <lemon.zhang@servasoft.com>
2019-08-23 18:40:27 +0800

    添加从git仓库拉取代码，并根据问题修改package内容；优化预设问题；

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const version = require('./package').version; // 版本号
 const program = require('commander'); // 命令行解析
 const createProgramFs = require('./lib/create-program-fs'); // 创建项目文件
 
+console.log(version);
+
 program.version(version, '-v, --version');
 
 program
-    .command('create <name>')    
-    .description('创建页面或组件')
-    .action((name) => createProgramFs(name));
+  .command('create <name>')    
+  .description('创建页面或组件')
+  .action((name) => createProgramFs(name));
 
 /* 后续可以根据不同的命令进行不同的处理，可以简单的理解为路由 */
 // program
